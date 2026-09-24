97d0e8d7dbbcb1e91a1c90862a827090dbf792d7
Kingsley Darko <kingsley_oteng@outlook.com>
2026-09-24 19:36:10 -0400

    added StackTest.java

--- DSA Assignments/Assignment1/IntSorter.java
+++
@@ -1,6 +0,0 @@
-public interface IntSorter {
-  public void init(int[] array);
-  public void sort();
-  public int getSwaps();
-  public long getRuntimeInNanos();
-}
---
+++ DSA/StackTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class StackTest{
+    Stack st;
+    
+    @before
+    public void init(){
+        st = new Stack();
+    }
+}
